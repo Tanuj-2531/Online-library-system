60ce43e4e361aaec60cd703b814ee3ac90530e89
Tanuj Agarwal <tanujag.2531@gmail.com>
2026-01-31 01:24:19 +0530

    Configured Redux store and books slice

---
+++ src/features/books/booksSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+import booksData from "../../data/booksData";
+
+const booksSlice = createSlice({
+  name: "books",
+  initialState: booksData,
+  reducers: {
+    addBook: (state, action) => { state.unshift(action.payload); },
+  },
+});
+
+export const { addBook } = booksSlice.actions;
+export default booksSlice.reducer;
